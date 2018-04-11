eb84020ac09628b70131468912631d1ca0be4ae2
BradSchoenrock <brads@ucar.edu>
2018-04-11 12:02:36 -0600

    fixing file vs directory copying
    Former-commit-id: 88eb42fe05dd36b25ac507742c43bc7b2d424f99

--- WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFScript.py
+++ WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFScript.py
@@ -1612,9 +1612,9 @@ def main():
     # creating Error file variable for use if needed ... which of course it never will be ... right? 
     ErrorFile = os.path.join(sys.argv[1],"Data","NetCDFChild","NetCDFPythonErrors_"+str(NowDate)+"_"+str(NowTime)+".txt")
 
-    print ("Error file loction if needed = ", ErrorFile)
+    print ("Loction of error file if needed = ", ErrorFile)
     
-    LocalOutputPath = os.path.join(sys.argv[1],"Data")
+    LocalOutputPath = os.path.join(sys.argv[1],"Data","")
     print ("Data saving to : ",LocalOutputPath)
     if os.path.isdir(LocalOutputPath): # the first should be the directory where the Data folder is located.
 
@@ -1624,7 +1624,7 @@ def main():
 
         if os.path.isdir(sys.argv[2]):
             #ensure output directory exists
-            OutputPath = os.path.join(sys.argv[2],"Data")
+            OutputPath = os.path.join(sys.argv[2],"Data","")
             ensure_dir(OutputPath)
         else:
             writeString = "WARNING: argument 2 (path to external hard drive to copy data onto) - "+sys.argv[2]+" - is not a valid directory to write to. Writing to local data directory instead. - "+str(NowTime) + '\n'
@@ -1713,7 +1713,7 @@ def main():
                 data_dirs_list = os.listdir(LocalOutputPath)
                 #print (data_dirs_list)
                 for data_dir in data_dirs_list:
-                    print ("Copying",data_dir, datetime.datetime.utcnow().strftime("%H:%M:%S"))
+                    print ("Copying hardware level",data_dir, datetime.datetime.utcnow().strftime("%H:%M:%S"))
                     if os.path.isfile(os.path.join(LocalOutputPath,data_dir)):
                         shutil.copy(os.path.join(LocalOutputPath,data_dir), os.path.join(OutputPath,data_dir))
                     else:
@@ -1723,13 +1723,22 @@ def main():
                                 shutil.copy(os.path.join(LocalOutputPath,data_dir,day_dir), os.path.join(OutputPath,data_dir,day_dir))
                             else:
                                 if day_dir >= ThenDate:
-                                    print ("Copying day", day_dir, datetime.datetime.utcnow().strftime("%H:%M:%S"))
+                                    print ("Copying day level ", day_dir, datetime.datetime.utcnow().strftime("%H:%M:%S"))
                                     LocalCopyFrom = os.path.join(LocalOutputPath,data_dir,day_dir)
-                                    src_file_names = os.listdir(os.path.join(LocalOutputPath,data_dir,day_dir))
+                                    src_file_names = ""
+                                    if os.path.isfile(os.path.join(LocalOutputPath,data_dir,day_dir)):
+                                        fromi = os.path.join(LocalCopyFrom,os.path.join(LocalOutputPath,data_dir,day_dir))
+                                        toi = os.path.join(OutputPath,data_dir,day_dir)
+                                        ensure_dir(os.path.join(OutputPath,data_dir,""))
+                                        shutil.copy(fromi,toi)
+                                    else:
+                                        src_file_names = os.listdir(os.path.join(LocalOutputPath,data_dir,day_dir))
                                     ensure_dir(os.path.join(OutputPath,data_dir,day_dir,""))
                                     for file_name in src_file_names:
                                         if (os.path.isfile(os.path.join(LocalCopyFrom,file_name))):
-                                            shutil.copy(os.path.join(LocalCopyFrom,file_name), os.path.join(OutputPath,data_dir,day_dir,""))
+                                            fromt = os.path.join(LocalCopyFrom,file_name)
+                                            tot = os.path.join(OutputPath,data_dir,day_dir,"")
+                                            shutil.copy(fromt,tot)
 
     # if os.path.isdir(os.path.join(sys.argv[1],"Data"):        
     else:
