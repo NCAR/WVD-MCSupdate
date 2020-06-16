01aba0d5937daafbaa0fb1a89b891dc4aeec446d
stillwer <stillwel@ucar.edu>
2020-06-16 22:31:20 +0000

    Fixing bugs (was sending responses not commands). Also, removed WS from function names aiming at an object oriented concept eventually. ~RS

--- WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/SocketComms/WeatherStationTCP.py
+++ WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/SocketComms/WeatherStationTCP.py
@@ -1,10 +1,10 @@
 
 from CommsClass import TCPComms as TCP
 
-def WeatherStationProbe(IPAddress='192.168.0.111',Port=4760,Timeout=1,Body=300,Term=b"\r"):
+def WeatherStationProbe(IPAddress='192.168.0.110',Port=4760,Timeout=1,Body=30,Term=b"\r"):
     # Defining commands to be sent
-    CommList  = [b"$ 28763 M 00160\r", b"$ 28763 M 00260\r", 
-                 b"$ 28763 M 00360\r", b"$ 28763 M 00270\r"]
+    CommList  = [b"& 28673 M 00160\r", b"& 28673 M 00260\r", 
+                 b"& 28673 M 00360\r", b"& 28673 M 00270\r"]
     Range     = [110.,100.,900.,1000.]
     Offset    = [-50.,  0.,300.,   0.]
     # Initializing the communication class
@@ -12,31 +12,30 @@ def WeatherStationProbe(IPAddress='192.168.0.111',Port=4760,Timeout=1,Body=300,T
     # Send the communications
     _ , Response, Error = S.Communicate(CommList)
     # Parsing and understanding the response
-    ResponseList = WeatherStationCheckResponse(CommList,Response,Error,Term,Range,Offset)
+    ResponseList = CheckResponse(CommList,Response,Error,Term,Range,Offset)
     return(ResponseList)
     
-def WeatherStationCheckResponse(CommList,Response,Error,Term,Range,Offset):
+def CheckResponse(CommList,Response,Error,Term,Range,Offset):
     # Checking the weather station response
     if Error is None :
         # No error occurred
-        ResponseList = WeatherStationDecode(CommList, Response,Term,Range,Offset)
+        return(Decode(CommList, Response,Term,Range,Offset))
     else:
         # An error occured so make a fake response
         if   Error == 'Error in the Initialization method':  ErrorCode = -2001
         elif Error == 'Error in the Send method':            ErrorCode = -2002
         elif Error == 'Error in the Read method':            ErrorCode = -2003
         else:                                                ErrorCode = -2000       
-        ResponseList = [ErrorCode]*len(CommList)
-    return(ResponseList)
+        return([ErrorCode]*len(CommList))
     
-def WeatherStationDecode(Commands,Responses,TermChar,Ranges,Offsets):
+def Decode(Commands,Responses,TermChar,Ranges,Offsets):
     # Looping over all of the responses and checking that they are reasonable
-    # Note the weather station echos the command
+    # Note: weather station echos the command with a different init character
     Value = []
     for Comm,Resp,Range,Offset in zip(Commands,Responses,Ranges,Offsets):
-        # Stripping off the termination characters
-        Comm = Comm.decode("utf-8").replace(TermChar.decode("utf-8"),'')
-        Resp = Resp.decode("utf-8").replace(TermChar.decode("utf-8"),'')
+        # Stripping off the initiation and termination characters
+        Comm = Comm.decode("utf-8").replace(TermChar.decode("utf-8"),'')[1:]
+        Resp = Resp.decode("utf-8").replace(TermChar.decode("utf-8"),'')[1:]
         # Checking for the command echo and converting to a physical value
         try:
             Resp = int(Resp.replace(Comm,'')) if Comm in Resp else -99
