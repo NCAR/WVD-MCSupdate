616e4197d5a20b644ffba54e0ad0673b71bc5601
stillwer <stillwel@ucar.edu>
2021-03-17 20:28:22 +0000

    Replacing 2 rsync scripts with single one with same functionality. ~RS

--- WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/SyncBackup.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import SharedPythonFunctions as SPF
-
-def DoRSync(syncFrom, syncTo,WarningFile,ErrorFile):
-    process = subprocess.Popen(["C:\\Program Files (x86)\\ICW\\bin\\rsync.exe", "-avz", "--compress-level=9", 
-                                SPF.convertString2CWSyntax(syncFrom)+ '/Data/', 
-                                SPF.convertString2CWSyntax(syncTo)],stdout=subprocess.PIPE)
-    stdout = process.communicate()[0]
-    return stdout.decode('utf-8')
---
+++ WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/rsync.py
@@ -0,0 +1,23 @@
+# Written By: Robert Stillwell
+# Written For: National Center for Atmospheric Research
+# This set of functions is used to call data rsyncing from labview. If the 
+# program is not called directly, it assumes that the user just wants to backup
+# all MPD data to the standard backup drive location. 
+
+import subprocess, SharedPythonFunctions as SPF
+
+def DoRSync(syncFrom, syncTo,WarningFile=None,ErrorFile=None):
+    process = subprocess.Popen(["C:\\Program Files (x86)\\ICW\\bin\\rsync.exe", "-avz", "--compress-level=9", 
+                                SPF.convertString2CWSyntax(syncFrom)+ '/Data/', 
+                                SPF.convertString2CWSyntax(syncTo)],stdout=subprocess.PIPE)
+    stdout = process.communicate()[0]
+    return stdout.decode('utf-8')
+
+
+if __name__ == '__main__':
+    # Default directories to rsync
+    syncFrom = '/cygdrive/c/Users/h2odial/WVD-MCSupdate/WVDNewArchitectureUpdate/WVD_Architecture_Update/Data/'
+    syncTo   = '/cygdrive/d/MPDBackup/'
+    # Performing the rsyncing with no logging
+    DoRSync(syncFrom, syncTo)
+
