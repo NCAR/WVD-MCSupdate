5c76aa0b09b215a0771bf2d4c464b0bef00339e3
stillwer <stillwel@ucar.edu>
2021-04-07 19:04:01 +0000

    Shortening to make huge scripts more managable to read. ~RS

--- WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/MakeChildFilesV3.py
+++ WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/MakeChildFilesV3.py
@@ -60,13 +60,10 @@ def makeNetCDF(ThenDate,ThenTime,NowDate,NowTime,LastTime,WarningFile,ErrorFile,
                 # Looking for files and looping over them
                 FileList = SPF.getFiles(Where2FindData , FileBase, FileExt, ThenDate, ThenTime)               
                 for File in FileList:
-#                    processGeneral(FolderType,FileType,File,NetCDFPath,Header) 
                     try:
                         processGeneral(FolderType,FileType,File,NetCDFPath,Header) 
                     except:
-                        print('Failed')
-                        writeString = 'WARNING: Failure to process ' + FolderType + ' data - ' + \
-                                      FolderType + ' file = ' + str(File) + ' - ' + str(NowTime) + \
-                                      '\n' + str(sys.exc_info()[0]) + '\n\n'
+                        writeString = 'WARNING: Failure to process ' + FolderType + ' data - ' + FolderType +\
+                                      ' file = ' + str(File) + ' - ' + '\n' + str(sys.exc_info()[0]) + '\n\n'
                         print(writeString)
                         
