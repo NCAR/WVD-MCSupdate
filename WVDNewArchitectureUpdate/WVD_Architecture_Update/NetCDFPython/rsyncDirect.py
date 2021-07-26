ef452f12d86c7badae65ac0f1d4b30974819184c
stillwer <stillwel@ucar.edu>
2021-07-26 13:40:15 +0000

    Adding comments to rsync direct function. ~RS

--- WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/rsyncDirect.py
+++ WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/rsyncDirect.py
@@ -1,5 +1,8 @@
-
-
+# Written By: Robert Stillwell
+# Written For: National Center for Atmospheric Research
+# This set of functions is used to call data rsyncing from labview. It just allows
+# the user to see the rsync output directly and doesn't have to wait until process
+# return. 
 
 import subprocess
 
