7fb55f0c5483d36dee391f958d72391ea33d7ab4
stillwer <stillwel@ucar.edu>
2023-11-17 19:34:07 +0000

    Adding both drives to rsyncing. ~RS

--- WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/rsyncDirect.py
+++ WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/rsyncDirect.py
@@ -7,6 +7,6 @@
 import subprocess
 
 syncFrom = '/cygdrive/c/Users/h2odial/WVD-MCSupdate/WVDNewArchitectureUpdate/WVD_Architecture_Update/Data/'
-syncTo   = '/cygdrive/d/MPDBackup/'
+syncTo   = '/cygdrive/e/MPDBackup/'
 
 subprocess.call(["C:\\Program Files (x86)\\ICW\\bin\\rsync.exe", "-av", syncFrom, syncTo])
