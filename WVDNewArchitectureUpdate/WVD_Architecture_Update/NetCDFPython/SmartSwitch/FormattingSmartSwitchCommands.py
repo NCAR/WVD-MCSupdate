f8cbc8b3951c3507da8f51dfb124052eab65995f
stillwer <stillwel@ucar.edu>
2024-02-22 21:22:49 +0000

    Updating SmartSwitch definitions to run 2 different gate lengths. ~RS

--- WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/SmartSwitch/FormattingSmartSwitchCommands.py
+++ WVDNewArchitectureUpdate/WVD_Architecture_Update/NetCDFPython/SmartSwitch/FormattingSmartSwitchCommands.py
@@ -232,20 +232,30 @@ if __name__ == '__main__':
       A.dacA = 52428
       A.dacB = 65535
       PulseDefs.append(A)
-    
+
       # Timer 1 (Short Pulses)
-      B.Timers[Timers.Timer_0.value].SetFullPulse(300, 20, 1)  # WV TSOA
-      B.Timers[Timers.Timer_1.value].SetFullPulse(300, 30, 1)  # On/Off TWA
-      B.Timers[Timers.Timer_2.value].SetFullPulse(290, 40, 1)  # Gate
+      B.Timers[Timers.Timer_0.value].SetFullPulse(300,100, 1)  # WV TSOA
+      B.Timers[Timers.Timer_1.value].SetFullPulse(300,110, 1)  # On/Off TWA
+      B.Timers[Timers.Timer_2.value].SetFullPulse(290,130, 1)  # Gate
       B.Timers[Timers.Timer_3.value].SetFullPulse(300,100, 1)  # Sync 1
-#      B.Timers[Timers.Timer_4.value].SetFullPulse(300, 100, 1) # Sync 0
-      B.Timers[Timers.Timer_5.value].SetFullPulse(300, 20, 1)  # O2 TSOA (Gate Temp)
-#      B.ControlFlags = (1<<2) + (1<<6) + (0<<7) + (1<<20);     # Flag 2 & 20: Timer 2 Inverted,
+      B.Timers[Timers.Timer_5.value].SetFullPulse(300,100, 1)  # O2 TSOA (Gate Temp)
       B.ControlFlags = (1<<2) + (0<<6) + (1<<7) + (1<<20);     # Flag 2 & 20: Timer 2 Inverted,
                                                                # Flag 6: RF1, Flag 7: RF2
       B.dacA = 52428
       B.dacB = 65535
       PulseDefs.append(B)
+
+#      # Timer 1 (Short Pulses)
+#      B.Timers[Timers.Timer_0.value].SetFullPulse(300, 20, 1)  # WV TSOA
+#      B.Timers[Timers.Timer_1.value].SetFullPulse(300, 30, 1)  # On/Off TWA
+#      B.Timers[Timers.Timer_2.value].SetFullPulse(290, 40, 1)  # Gate
+#      B.Timers[Timers.Timer_3.value].SetFullPulse(300,100, 1)  # Sync 1
+#      B.Timers[Timers.Timer_5.value].SetFullPulse(300, 20, 1)  # O2 TSOA (Gate Temp)
+#      B.ControlFlags = (1<<2) + (0<<6) + (1<<7) + (1<<20);     # Flag 2 & 20: Timer 2 Inverted,
+#                                                               # Flag 6: RF1, Flag 7: RF2
+#      B.dacA = 52428
+#      B.dacB = 65535
+#      PulseDefs.append(B)
       
       # Timer 2 (Scan Mode)
       #C.Timers[Timers.Timer_0.value].SetFullPulse(300, 0, 1) # WV TSOA
