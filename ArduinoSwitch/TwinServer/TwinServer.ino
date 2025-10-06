db94e1a1b1213b9a26631c1b68501926203282bd
stillwer <stillwel@ucar.edu>
2025-10-06 11:52:34 -0700

    Updating h command to activate pin 6 (hygrostat) instead of pin 2 (shutter). ~RS

--- ArduinoSwitch/TwinServer/TwinServer.ino
+++ ArduinoSwitch/TwinServer/TwinServer.ino
@@ -125,7 +125,7 @@ void executeRequest(EthernetClient* client, String* request){
   else if('h' == command || 'S' == command) {
     String writeVals = request->substring(1,2);
     int pinState = writeVals.charAt(0);
-    int pinNum = 2;
+    int pinNum = 6;
     if ('h' == command){
       int pinNum = 6;
     }
